83588df83efd8e569c3eb0b72067027543935da6
Dharuan Emina <dharuan.emina@compasso.com.br>
2021-07-04 17:07:13 -0300

    Added router and json body requests configuration

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import "./database"; // import and start connections
+import { router } from "./router";
 
 const app = express();
+app.use(express.json());
+
+app.use(router);
 
 app.listen(3000, () => console.log("Server is running..."));
