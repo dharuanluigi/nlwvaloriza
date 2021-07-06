52ac3f94274fa377bed0795ab18311824b962e8b
Dharuan Luigi <dharuanluigi@gmail.com>
2021-07-05 22:52:42 -0300

    Added comments

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,11 @@ import "./database"; // import and start connections
 import { router } from "./router";
 
 const app = express();
+
+// configs
 app.use(express.json());
 
+// access
 app.use(router);
 
 app.listen(3000, () => console.log("Server is running..."));
